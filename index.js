332e0ec94a6c9e2dda3893e120e3ba732ff953cb
Catrina Friday <cat.friday@gmail.com>
2020-08-13 20:44:14 -0400

    updated index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 document.addEventListener("DOMContentLoaded", function(e){
     horoscopeUrl = "http://localhost:3000/horoscopes/"
     usersUrl = "http://localhost:3000/users/"
+    userHoroscopeUrl = "http://localhost:3000/user_horoscopes/"
     
     fetch(horoscopeUrl)
     .then(response => response.json())
@@ -33,7 +34,6 @@ document.addEventListener("DOMContentLoaded", function(e){
         form.addEventListener("submit", (e) => {
             main.appendChild(div)
             e.preventDefault()
-            console.log(e)
             createUser(form)
             form.reset()
     })
@@ -44,6 +44,10 @@ document.addEventListener("DOMContentLoaded", function(e){
         let month = document.querySelector('#dob-month').value
         let year = document.querySelector('#dob-year').value
         let birthday = year.concat(month, bday)
+        user = {
+            name: form.name.value,
+            birthdate: birthday
+        }
         fetch(usersUrl, {
             method: "POST",
             headers: {
@@ -51,11 +55,57 @@ document.addEventListener("DOMContentLoaded", function(e){
             "Accept": "application/json"
         },
         body: JSON.stringify({
-            name: form.name.value,
-            birthdate: birthday
+            // 
+            user 
         })
     })
     .then(response => response.json())
-    .then(horoscope => renderHoroscope(horoscope))
+    .then(horoscope => 
+        renderHoroscope(horoscope),
+        userHoroscope(user, horoscope))
 }
+
+function userHoroscope(user, horoscope){
+   
+    fetch(userHoroscopeUrl, {
+        method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            user_id: user.id,
+            horoscope_id: horoscope.id
+        })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data),
+    writeHoroscope(user)
+    )
+}
+
+function writeHoroscope(user){
+    let newForm = document.createElement('form')
+    newForm.className = 'add-horo-form'
+    let formDiv = document.createElement('div')
+    formDiv.id = 'new-horo'
+    newForm.appendChild(formDiv)
+    let h3 = document.createElement('h3')
+    h3.innerText = "Leave a Horoscope for Someone Else!"
+    let label = document.createElement('label')
+    label.innerText = 'Horoscope Title'
+    formDiv.appendChild(label)
+    let input = document.createElement('input')
+    input.innerHTML = `
+    type="text"
+    name="title"
+    value=""
+    placeholder="Enter Title"
+    class="input-text"`
+    
+    console.log(newForm)
+}
+
+
+
 })
